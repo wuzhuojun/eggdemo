023185f228f3a4eca66733d7c24a7c774b3d435e
wuzhuojun <wu_zhuojun@sina.com>
2017-09-16 20:38:23 +0800

    egg demo

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1505564436334_832';
+
+  // add your config here
+
+  return config;
+};
